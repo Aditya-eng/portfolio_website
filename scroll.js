8b3cf54a465dd7f118f6287bd7c91701db47f978
Aditya garg <58650336+Aditya-eng@users.noreply.github.com>
2023-10-01 18:18:31 +0530

    add4

---
+++ scroll.js
@@ -0,0 +1,12 @@
+// $('#about').mousedown(function(){
+//     timeout = setInterval(function(){
+//         window.scrollBy(0,1); // May need to be -1 to go down
+//     }, 0); // Play around with this number. May go too fast
+
+//     return false;
+// });
+
+// $(document).mouseup(function(){
+//     clearInterval(timeout);
+//     return false;
+// });
